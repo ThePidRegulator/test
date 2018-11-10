3cf5800d8c0ffcee52951199ed608d0f936a35c8
Henrik <Henrik.rudi@gmail.com>
2018-11-10 05:02:32 +0100

    Initial definition of what SatOrbitEnvironmentSim will be
    Definition written in main.m file

---
+++ SatOrbitEnvironmentSim/main.m
@@ -0,0 +1,13 @@
+%What is Main going to contain
+%Description of what main dose 
+%Main will be where different orbit cenarios will can be defined
+%Once a orbit scenario is simulated it will be stored in a format that allows for
+%faster SatASDC simulation
+%This should be capable of simulating:
+%Specific orbit
+%Sun Sat iraidiance
+%Earth Sat iradiance
+%Magnetic field Vector
+%Gravity Vector
+%Other environmental torquers on Orientation
+
