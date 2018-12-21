707e135421a01ef836b6ef4d7363a321735d4a6d
Henrik <Henrik.rudi@gmail.com>
2018-12-22 00:56:55 +0100

    Initial implemHentation of time stepping
    Goals:
    Stepping size delta time=d_time must be flexible.
    Stepping must be precise over years.
    Stepping must handle all the irregularities of years months and weeks in UTC 
    Date Time.
    Challenges.
    Conversion between a easily increment-able format like Julian Days and Date Time UTC is not precise.
    
    By doing stepping in "Julian Days" time format and converting to/from the Date Time UTC struct, all the tedious additions of extra days and weeks in the moths in years is handled by the conversion function. The only additional functionality needed is a correction of the conversion error. 
    
    d_Jd created from:
    d_Jd = timeDatetime2jd(step_timeUTC); %using a NONCONVENTIONAL(evil) UTC date that converts timeUTC.sec=1 to a approximate Jd time of 1 sec for stepping OR:
    d_Jd=timeDatetime2jd(step_timeUTC)-timeDatetime2jd(start_timeUTC);% This methods with a CONVENTIONAL(nice) timeUTC struct, give the same result.
    
    Over weeks months and years, d_Jd is to small for stepping precisely.   Initial tests gave a correction of 
    d_Jd = d_Jd+ 0.00000627779999e-05; %for 1second steps
    This seems to give stable results over some 20 years at least and the error seems to be consistently smaller than 0.01 sec. 
    Another way is to round timeUTC.sec to a clean flat second after stepping and conversion of Jd as done in line 65 and 64.

--- SatOrbitEnvironmentSim/main.m
+++ SatOrbitEnvironmentSim/main.m
@@ -4,10 +4,88 @@
 %Once a orbit scenario is simulated it will be stored in a format that allows for
 %faster SatASDC simulation
 %This should be capable of simulating:
-%Specific orbit
-%Sun Sat iraidiance
-%Earth Sat iradiance
-%Magnetic field Vector
-%Gravity Vector
 %Other environmental torquers on Orientation
+
+%goal number one get the old sun sensor sim upp
+
+%how much should i hide in this main function
+
+%the satelite needs to
+%SENS
+%magvect
+%sunvect
+%rotation velocity
+%DISTURBANCE
+%earth_refl_sunvect
+%aerodynamic torque
+%gravity
+%solar pressure
+%SIMULATION FACTS
+%timeUTC actual
+%
+%
+
+addpath(genpath("D:/Users/UrgST/Documents/GitRepos/test",".git"))
+run constants.m
+
+%initiating sim start time struct
+start_timeUTC.year  =   1980;
+start_timeUTC.mon   =      4;
+start_timeUTC.day   =      4;
+start_timeUTC.hr    =      1;
+start_timeUTC.min   =      0;
+start_timeUTC.sec   =    24.0000;
+
+%initiating sim stop time struct
+stop_timeUTC.year  =   1980;
+stop_timeUTC.mon   =      4;
+stop_timeUTC.day   =      20;
+stop_timeUTC.hr    =      2;
+stop_timeUTC.min   =      1;
+stop_timeUTC.sec   =   24.00;
+
+
+%initiating step size is d_jd = step_time_Jd-start_time_Jd
+step_timeUTC.year  =      1980;
+step_timeUTC.mon   =      4;
+step_timeUTC.day   =      4;
+step_timeUTC.hr    =      1;
+step_timeUTC.min   =      20;
+step_timeUTC.sec   =      24.00;
+
+d_Jd           = timeDatetime2jd(step_timeUTC)-timeDatetime2jd(start_timeUTC);      % + 0.00000627779999e-05; %this stabelizes the steping in seconds for many years
+stop_time_Jd   = timeDatetime2jd(stop_timeUTC);
+start_time_Jd  = timeDatetime2jd(start_timeUTC);
+
+satrec = orbitTwoline2rv(tle);
+
+for Jd = start_time_Jd:d_Jd:stop_time_Jd
+
+  timeUTC                       = timeJd2datetime(Jd);                              % may need a stabelizing function for given step d_Jd, conversion of 1 second to Jd is not precise enouf for stepping;
+  timeUTC.sec                   = round(timeUTC.sec*10)/10;                         % this stabelizes the stepping 
+  Jd                            = timeDatetime2jd(timeUTC);
+  
+  [satrec2,r_sat_eci,v_sat_eci]   = orbitSgp4(satrec,timeUTC);
+  
+  Beci                          = magIgrf(r_sat_eci,timeUTC);
+  
+  r_sun_eci                     = sun(timeUTC);
+  r_sun_ecef                    = frameEci2ecef(r_sun_eci,timeUTC);
+  r_sat_ecef                    = frameEci2ecef(r_sat_eci,timeUTC);
+  
+  %albedo                        = albedo_wrapper(r_sat_ecef,r_sun_ecef,param,redfac,refllib);
+  %albedo_v                      = CreateAlbedoVectors(r_sat_ecef,Albedo_ecef);
+  
+  %gravity vector      variations to small to matter
+  
+  %aerodynamic torque  size and shape of satelite
+  
+  %solar prezure       size and shape of satelite
+  
+  timeUTC.mon
+  timeUTC.day
+  timeUTC.min
+  
+end
+
 
