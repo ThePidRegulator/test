9e9c3e2cced4271ffaac415e99f99ca771758f31
Henrik <Henrik.rudi@gmail.com>
2019-06-23 03:29:12 +0200

    Changes gridangle to use the new radiMap

--- SatEnvironmentSim/Models/AlbedoToolbox-1.0/private/gridangle.m
+++ SatEnvironmentSim/Models/AlbedoToolbox-1.0/private/gridangle.m
@@ -4,10 +4,12 @@
 %
 % $Id: gridangle.m,v 1.3 2006/05/17 14:39:17 danji Exp $
 
-function rho = gridangle(i1,j1,i2,j2,sy,sx);
+function rho = gridangle(i1,j1,i2,j2,radiMap);
 
-[theta1,phi1] = idx2rad(i1,j1,sy,sx);
-[theta2,phi2] = idx2rad(i2,j2,sy,sx);
+phi1 = radiMap(1,i1);
+phi2 = radiMap(1,i2);
+theta1 = radiMap(2,j1);
+theta2 = radiMap(2,j2);
 
 rho = acos(sin(phi1)*sin(phi2)*cos(theta1-theta2)+cos(phi1)*cos(phi2));
 
