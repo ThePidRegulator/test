d827e6d06a415a7e905f06d22450204954f5af5f
Henrik <Henrik.rudi@gmail.com>
2019-06-23 03:29:12 +0200

    Revert "Changes gridangle to use the new radiMap"
    This reverts commit c36ae2fa6053ce2268da1600daf0c86db34b55cf.

--- SatEnvironmentSim/Models/AlbedoToolbox-1.0/gridangle.m
+++ SatEnvironmentSim/Models/AlbedoToolbox-1.0/gridangle.m
@@ -4,12 +4,10 @@
 %
 % $Id: gridangle.m,v 1.3 2006/05/17 14:39:17 danji Exp $
 
-function rho = gridangle(i1,j1,i2,j2,radiMap);
+function rho = gridangle(i1,j1,i2,j2,sy,sx);
 
-phi1 = radiMap(1,i1);
-phi2 = radiMap(1,i2);
-theta1 = radiMap(2,j1);
-theta2 = radiMap(2,j2);
+[theta1,phi1] = idx2rad(i1,j1,sy,sx);
+[theta2,phi2] = idx2rad(i2,j2,sy,sx);
 
 rho = acos(sin(phi1)*sin(phi2)*cos(theta1-theta2)+cos(phi1)*cos(phi2));
 
